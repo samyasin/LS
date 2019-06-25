e78ce30ce1460d2f551534b99f963e3c6c8a00bc
Noor Aldeen <nooralazzeh97@gmail.com>
2019-06-25 16:49:45 +0300

    change admin / create category page
    admin edit
    add category
    remove category
    edit category

--- admin/admin.php
+++ admin/admin.php
@@ -1,15 +1,13 @@
 <?php
-session_start();
 include 'includes/config.php';
 global $con;
 $msg = "";
 if(isset($_POST['add'])){
   $fallname = $_POST['fallname'];
   $email    = $_POST['email'];
+  /* Encrypt a password */
   $password = md5($_POST['password']);
 
-  /* Encrypt a password */
-  //$password = md5($password);
 
   /* check if user exist */
   $sql="SELECT email FROM admin WHERE email='$email' AND fallname='$fallname'";
@@ -21,7 +19,7 @@ if(isset($_POST['add'])){
     $insert = "INSERT INTO admin(fallname,email,password) VALUES
               ('$fallname','$email','$password')";
     if(mysqli_query($con,$insert)){
-      getAccount($email,$password);
+    //  getAccount($email,$password);
     }
     header('Location: admin.php');
 
@@ -35,47 +33,27 @@ if(isset($_POST['remove'])){
   header('Location: admin.php');
 }
 
+if(isset($_POST['edit'])){
+  $id       = $_POST['id'];
+  $name     = $_POST['fallname'];
+  $email    = $_POST['email'];
+  $password = $_POST['password'];
+
+
+  if($password == ''){
+    $update   = "UPDATE admin SET fallname='$name', email='$email' WHERE id_admin='$id'";
+    $result = mysqli_query($con,$update);
+  } else {
+    $password = md5($password);
+    $update   = "UPDATE admin SET fallname='$name', email='$email', password='$password' WHERE id_admin='$id'";
+    $result = mysqli_query($con,$update);
+  }
+  header('Location: admin.php');
+}
 
 ?>
 
-
-
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ela Admin - HTML5 Admin Template</title>
-    <meta name="description" content="Ela Admin - HTML5 Admin Template">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
-    <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
-
-    <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
-
-</head>
-
-<body>
-
-  <?php include 'header.php'; ?>
-
+  <?php include 'includes/header.php'; ?>
 
   <!-- Content -->
   <div class="content">
@@ -83,35 +61,36 @@ if(isset($_POST['remove'])){
     <div class="animated fadeIn">
 
       <!-- add admin -->
-
       <div class="row">
         <div class="col-12">
             <div class="card">
-                <div class="card-header">Add Admin</div>
+                <div class="card-header">
+                  <strong class="card-title">Add Admin</strong>
+                </div>
                 <div class="card-body card-block position-relative">
-                    <form id="add_admin"action = "admin.php" method="POST" class="">
+                    <form id="add_admin" action = "admin.php" method="POST" class="">
                         <div class="form-group">
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                                <input type="text" id="fallname" name="fallname" placeholder="Fall Name" class="form-control">
+                                <input type="text" id="fallname" name="fallname" placeholder="Fall Name" class="form-control" required autocomplete="off">
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
-                                <input type="email" id="email" name="email" placeholder="Email" class="form-control">
+                                <input type="email" id="email" name="email" placeholder="Email" class="form-control" required autocomplete="off">
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-                                <input type="password" id="password" name="password" placeholder="Password" class="form-control">
+                                <input type="password" id="password" name="password" placeholder="Password" class="form-control" required autocomplete="off">
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-                                <input type="password" id="Cpassword" name="Cpassword" placeholder="Confirm Password" class="form-control">
+                                <input type="password" id="Cpassword" name="Cpassword" placeholder="Confirm Password" class="form-control" required autocomplete="off">
                             </div>
                         </div>
                         <div class="form-actions form-group"><button type="submit" class="btn btn-success btn-sm" name="add" value="add">Submit</button></div>
@@ -153,24 +132,79 @@ if(isset($_POST['remove'])){
                     if(mysqli_num_rows($resultRetrieve)>0){
                       $i=1;
                       while($row = mysqli_fetch_assoc($resultRetrieve)){
-                        echo "<tr>
-                          <td>".$i."</td>
-                          <td>".$row['fallname']."</td>
-                          <td>".$row['email']."</td>
+                        ?>
+                        <tr>
+                          <td><?php echo $i; ?></td>
+                          <td><?php echo $row['fallname'];?></td>
+                          <td><?php echo$row['email'];?></td>
                           <td>
-                          <form id='option".$i."'action = 'admin.php' method='POST'>
-                          <input type='hidden' name='id' value='".$row['id_admin']."'>
-                            <button class='btn btn-primary' type='submit' name='edite'>Edit</button>
+                          <!-- modal -->
+                          <!-- Button trigger modal -->
+                          <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#modal<?php echo $i;?>'>
+                            Edit
+                          </button>
+
+                          <!-- Modal -->
+                          <div class='modal fade' id='modal<?php echo $i;?>' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+                            <div class='modal-dialog modal-dialog-centered' role='document'>
+                              <div class='modal-content'>
+                                <div class='modal-header'>
+                                  <h5 class='modal-title d-inline' id='exampleModalLongTitle<?php echo $i;?>'>Edit Admin</h5>
+                                  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                                    <span aria-hidden='true'>&times;</span>
+                                  </button>
+                                </div>
+                                <div class='modal-body pb-0'>
+                                <!-- form edit admin -->
+                                <form id='edit_admin<?php echo $i;?>' action = 'admin.php' method='POST' class="">
+                                    <input type='hidden' name='id' value='<?php echo $row['id_admin'];?>'>
+                                    <div class='form-group'>
+                                        <div class='input-group'>
+                                            <div class='input-group-addon'><i class='fa fa-user'></i></div>
+                                            <input type='text' id='eFallname<?php echo $i;?>' name='fallname' placeholder='Fall Name' class='form-control' required autocomplete='off' value="<?php echo $row['fallname'];?>">
+                                        </div>
+                                    </div>
+                                    <div class='form-group'>
+                                        <div class='input-group'>
+                                            <div class='input-group-addon'><i class='fa fa-envelope'></i></div>
+                                            <input type='email' id='eEmail<?php echo $i;?>' name='email' placeholder='Email' class='form-control' required autocomplete='off' value="<?php echo $row['email'];?>">
+                                        </div>
+                                    </div>
+                                    <div class='form-group'>
+                                        <div class='input-group'>
+                                            <div class='input-group-addon'><i class='fa fa-asterisk'></i></div>
+                                            <input type='password' id='ePassword<?php echo $i;?>' name='password' placeholder='Password' class='form-control' autocomplete='off'>
+                                        </div>
+                                    </div>
+                                    <div class='form-group'>
+                                        <div class='input-group'>
+                                            <div class='input-group-addon'><i class='fa fa-asterisk'></i></div>
+                                            <input type='password' id='eCpassword<?php echo $i;?>' name='Cpassword' placeholder='Confirm Password' class='form-control' autocomplete='off'>
+                                        </div>
+                                    </div>
+                                    <div class='modal-footer'>
+                                      <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                                      <button type='submit' class='btn btn-primary' name="edit" value="edit">Save changes</button>
+                                    </div>
+                                </form>
+                                <!-- end form -->
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <!-- end modal -->
+                          <form id='option<?php echo $i;?>'action = 'admin.php' method='POST' class='d-inline'>
+                          <input type='hidden' name='id' value='<?php echo $row['id_admin'];?>'>
                             <button class='btn btn-danger' type='submit' name='remove' vlaue='remove'>Remove</button>
                           </form>
                           </td>
-                        </tr>";
+                        </tr>
+                        <?php
                         $i++;
                       }
                     }
                    ?>
                 </tbody>
-
               </table>
             </div>
           </div>
@@ -178,17 +212,13 @@ if(isset($_POST['remove'])){
       </div>
       <!-- / .display admin -->
 
-
     </div>
     <!-- / .Animated -->
 
   </div>
-
   <!-- /.content -->
 
-
-  <?php include 'footer.php'; ?>
-
+  <?php include 'includes/footer.php'; ?>
   <script>
       $(document).ready(function() {
   /*      $( "#add_admin" ).submit(function( event ) {
@@ -201,6 +231,5 @@ if(isset($_POST['remove'])){
         });*/
     } );
   </script>
-
 </body>
 </html>
---
+++ admin/category.php
@@ -0,0 +1,173 @@
+<?php
+include 'includes/config.php';
+global $con;
+$msg = "";
+if(isset($_POST['add'])){
+  $name = $_POST['category'];
+
+  /* check if user exist */
+  $sql="SELECT name FROM category WHERE name='$name'";
+  $result=mysqli_query($con,$sql);
+
+  if(mysqli_num_rows($result) > 0){
+    $msg ="This category is exist";
+  }else{
+    $insert = "INSERT INTO category(name) VALUES
+              ('$name')";
+    if(mysqli_query($con,$insert)){
+    //  getAccount($email,$password);
+    }
+    header('Location: category.php');
+
+  }
+}
+
+if(isset($_POST['remove'])){
+  $id      = $_POST['id'];
+  $delete  = "DELETE FROM category WHERE id_category='$id'";
+  $resulte = mysqli_query($con,$delete);
+  header('Location: category.php');
+}
+
+if(isset($_POST['edit'])){
+  $id       = $_POST['id'];
+  $name     = $_POST['category'];
+
+  $update   = "UPDATE category SET name='$name' WHERE id_category='$id'";
+  $result = mysqli_query($con,$update);
+
+  header('Location: category.php');
+}
+
+?>
+
+  <?php include 'includes/header.php'; ?>
+
+  <!-- Content -->
+  <div class="content">
+    <!-- Animated -->
+    <div class="animated fadeIn">
+
+      <!-- add admin -->
+      <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                  <strong class="card-title">Add Category</strong>
+                </div>
+                <div class="card-body card-block position-relative">
+                    <form id="add_admin" action = "category.php" method="POST" class="">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-th-list"></i></div>
+                                <input type="text" id="category" name="category" placeholder="Category" class="form-control" required autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="form-actions form-group"><button type="submit" class="btn btn-success btn-sm" name="add" value="add">Submit</button></div>
+                    </form>
+                    <?php if($msg!=""){ ?>
+                    <div class="alert alert-danger position-absolute w-50 p-2 text-center" style="right:10px;bottom:10px">
+                      <?php echo $msg; ?>
+                    </div>
+                  <?php } ?>
+                </div>
+            </div>
+        </div>
+      </div>
+      <!-- / .add admin -->
+
+      <!-- display admin -->
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <strong class="card-title">Category</strong>
+            </div>
+            <div class="card-body">
+              <table id="bootstrap-data-table" class="table table-striped table-bordered">
+
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Category</th>
+                    <th>Option</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <?php
+                    $retrieveData   = "SELECT * FROM category";
+                    $resultRetrieve = mysqli_query($con,$retrieveData);
+                    if(mysqli_num_rows($resultRetrieve)>0){
+                      $i=1;
+                      while($row = mysqli_fetch_assoc($resultRetrieve)){
+                        ?>
+                        <tr>
+                          <td><?php echo $i; ?></td>
+                          <td><?php echo $row['name'];?></td>
+                          <td>
+                          <!-- modal -->
+                          <!-- Button trigger modal -->
+                          <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#modal<?php echo $i;?>'>
+                            Edit
+                          </button>
+
+                          <!-- Modal -->
+                          <div class='modal fade' id='modal<?php echo $i;?>' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+                            <div class='modal-dialog modal-dialog-centered' role='document'>
+                              <div class='modal-content'>
+                                <div class='modal-header'>
+                                  <h5 class='modal-title d-inline' id='exampleModalLongTitle<?php echo $i;?>'>Edit Admin</h5>
+                                  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                                    <span aria-hidden='true'>&times;</span>
+                                  </button>
+                                </div>
+                                <div class='modal-body pb-0'>
+                                <!-- form edit admin -->
+                                <form id='edit_admin<?php echo $i;?>' action = 'category.php' method='POST' class="">
+                                    <input type='hidden' name='id' value='<?php echo $row['id_category'];?>'>
+                                    <div class='form-group'>
+                                        <div class='input-group'>
+                                            <div class='input-group-addon'><i class='fa fa-th-list'></i></div>
+                                            <input type='text' id='eCategory<?php echo $i;?>' name='category' placeholder='Category' class='form-control' required autocomplete='off' value="<?php echo $row['name'];?>">
+                                        </div>
+                                    </div>
+                                    <div class='modal-footer'>
+                                      <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                                      <button type='submit' class='btn btn-primary' name="edit" value="edit">Save changes</button>
+                                    </div>
+                                </form>
+                                <!-- end form -->
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <!-- end modal -->
+                          <form id='option<?php echo $i;?>'action = 'category.php' method='POST' class='d-inline'>
+                          <input type='hidden' name='id' value='<?php echo $row['id_category'];?>'>
+                            <button class='btn btn-danger' type='submit' name='remove' vlaue='remove'>Remove</button>
+                          </form>
+                          </td>
+                        </tr>
+                        <?php
+                        $i++;
+                      }
+                    }
+                   ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- / .display admin -->
+
+    </div>
+    <!-- / .Animated -->
+
+  </div>
+  <!-- /.content -->
+
+  <?php include 'includes/footer.php'; ?>
+</body>
+</html>
